b6aa910c982f46feeb9cab20b80405790f975a6c
rybaaa <bolshayaryba22@gmail.com>
2022-11-24 23:45:03 +0300

    ls52 refactoring users component to pure function

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,19 +5,28 @@ import axios from "axios";
 import avatar from '../../assets/img/avatar-svgrepo-com.svg'
 
 export const Users = (props: UsersPagePropsType) => {
-    if (!props.users.length) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response)=>{
-            props.setUsers(response.data.items)
-        })
+    const getUsers = () => {
+        if (!props.users.length) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
+                props.setUsers(response.data.items)
+            })
+        }
     }
+
+
     return (
         <div>
+            <button onClick={getUsers}>Get users</button>
             {props.users.map(u => <div key={u.id} className={s.block}>
                 <div className={s.avatar_btn_block}>
-                    <img src={u.photos?.small === null? avatar : u.photos.small}/>
+                    <img src={u.photos?.small === null ? avatar : u.photos.small}/>
                     {u.followed
-                        ? <button onClick={()=>{props.unfollow(u.id)} }>Unfollow</button>
-                        : <button onClick={()=>{props.follow(u.id)}}>Follow</button>}
+                        ? <button onClick={() => {
+                            props.unfollow(u.id)
+                        }}>Unfollow</button>
+                        : <button onClick={() => {
+                            props.follow(u.id)
+                        }}>Follow</button>}
                 </div>
                 <div className={s.user_block}>
                     <div className={s.user_block_info}>
