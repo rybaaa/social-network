2007124ef443e3583486c8b04ef2da098c9c715b
rybaaa <bolshayaryba22@gmail.com>
2022-11-28 23:29:41 +0300

    ls 53 users class component

--- src/components/Navbar/Nav.tsx
+++ src/components/Navbar/Nav.tsx
@@ -18,6 +18,9 @@ export const Nav = (props:SidebarPageType) => {
                 <NavLink to = '/messages' activeClassName={s.activeLink}> Messages</NavLink>
             </div>
             <div className={s.item}>
+                <NavLink to = '/users' activeClassName={s.activeLink}> Users</NavLink>
+            </div>
+            <div className={s.item}>
                 <NavLink to ='/news' activeClassName={s.activeLink}>News</NavLink>
             </div>
             <div className={s.item}>
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,44 +1,43 @@
 import React from 'react';
 import {UsersPagePropsType} from "./UsersContainer";
 import s from './Users.module.css'
-import axios from "axios";
 import avatar from '../../assets/img/avatar-svgrepo-com.svg'
+import axios from "axios";
 
-export const Users = (props: UsersPagePropsType) => {
-    const getUsers = () => {
-        if (!props.users.length) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
-                props.setUsers(response.data.items)
+export class Users extends React.Component <UsersPagePropsType> {
+    constructor(props:UsersPagePropsType) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items)
             })
-        }
     }
-
-
-    return (
-        <div>
-            <button onClick={getUsers}>Get users</button>
-            {props.users.map(u => <div key={u.id} className={s.block}>
-                <div className={s.avatar_btn_block}>
-                    <img src={u.photos?.small === null ? avatar : u.photos.small}/>
-                    {u.followed
-                        ? <button onClick={() => {
-                            props.unfollow(u.id)
-                        }}>Unfollow</button>
-                        : <button onClick={() => {
-                            props.follow(u.id)
-                        }}>Follow</button>}
-                </div>
-                <div className={s.user_block}>
-                    <div className={s.user_block_info}>
-                        <div>{u.name}</div>
-                        <div>{'u.location.country'},{'u.location.city'}</div>
+    render() {
+        return (
+            <div>
+                {this.props.users.map(u => <div key={u.id} className={s.block}>
+                    <div className={s.avatar_btn_block}>
+                        <img src={u.photos?.small === null ? avatar : u.photos.small}/>
+                        {u.followed
+                            ? <button onClick={() => {
+                                this.props.unfollow(u.id)
+                            }}>Unfollow</button>
+                            : <button onClick={() => {
+                                this.props.follow(u.id)
+                            }}>Follow</button>}
                     </div>
-                    <div>
-                        <div className={s.status}>{u.status}</div>
+                    <div className={s.user_block}>
+                        <div className={s.user_block_info}>
+                            <div>{u.name}</div>
+                            <div>{'u.location.country'},{'u.location.city'}</div>
+                        </div>
+                        <div>
+                            <div className={s.status}>{u.status}</div>
+                        </div>
                     </div>
-                </div>
-            </div>)}
-        </div>
-    );
+                </div>)}
+            </div>
+        );
+    }
 };
 
