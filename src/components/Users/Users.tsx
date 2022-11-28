75fc59aa17ece662585324e94e16ceb8a6678eb9
rybaaa <bolshayaryba22@gmail.com>
2022-11-28 23:55:56 +0300

    ls54 component did mount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,13 +5,13 @@ import avatar from '../../assets/img/avatar-svgrepo-com.svg'
 import axios from "axios";
 
 export class Users extends React.Component <UsersPagePropsType> {
-    constructor(props:UsersPagePropsType) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
     }
+
     render() {
         return (
             <div>
